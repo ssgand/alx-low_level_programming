46bcd9218641517d20cc3dc04b4775d1f48e05dc
Steve Gandhi <sstevegandhi@gmail.com>
2022-09-11 12:43:34 -0700

    Corrections 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,12 +4,13 @@
  * main - print alphabets in order
  *
  * this program prints in alphabets in order using the putchar keyword
+ * Return: 0
  */
 
 int main(void)
 {
 	char cr;
-	
+
 	for (cr = 'a'; cr <= 'z'; cr++)
 	{
 		putchar(cr);
