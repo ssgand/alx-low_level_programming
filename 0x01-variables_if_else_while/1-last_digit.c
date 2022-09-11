03623f79377f5a3e7c8ef6055c9241be8225240e
Steve Gandhi <sstevegandhi@gmail.com>
2022-09-11 04:36:10 -0700

    Program to calculate the remainder of a number and compare it to some other number

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* main - print the last digit of n
+ *
+ * Return: 0
+ */
+
+int main(void)
+
+{
+
+	int n;
+	int t;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	t= n % 10;
+	
+	if (t > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, t);
+	}
+	else if (t == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, t);
+	}
+	else if (t < 6)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, t);
+	}
+
+	return (0);
+
+}
