326264a492d2c683f005c44d635cc6e914fd3834
Steve Gandhi <sstevegandhi@gmail.com>
2022-09-11 12:31:39 -0700

    function that prints the alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+ * main - print alphabets in order
+ *
+ * this program prints in alphabets in order using the putchar keyword
+ */
+
+int main(void)
+{
+	char cr;
+	for (cr = 'a'; cr <= 'z'; cr++)
+	{
+		putchar(cr);
+	}
+	return (0);
+}
