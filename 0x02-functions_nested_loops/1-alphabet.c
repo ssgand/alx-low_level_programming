580b23b16f7562a8d124f3a481142a92707faebc
Steve Gandhi <sstevegandhi@gmail.com>
2022-09-13 12:32:11 -0700

    Corrections 3

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,19 +10,3 @@ int main(void)
 	print_alphabet();
 	return (0);
 }
-
-/**
- * print_alphabet - program to print alphabet in lower case
- *
- * no return
- */
-void print_alphabet(void)
-{
-	char cr;
-	
-	for (cr= 'a'; cr <= 'z'; cr++)                                                                                         
-	{
-		_putchar(cr);
-	}
-	_putchar('\n');
-}
