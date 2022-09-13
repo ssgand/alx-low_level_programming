afd948a449495bd73dbff35ae72e238dd40394fa
Steve Gandhi <sstevegandhi@gmail.com>
2022-09-13 12:28:31 -0700

    Corrections 2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,17 @@
 #include "main.h"
 
 /**
+ * main - printing lowercase alphabet with _putchar
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+/**
  * print_alphabet - program to print alphabet in lower case
  *
  * no return
