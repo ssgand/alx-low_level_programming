b3e5d65dab4d03154329e6c4864e03d8a7b35326
Steve Gandhi <sstevegandhi@gmail.com>
2022-09-12 17:13:05 -0700

    printing 3 digits without repetitions and in order

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - program to print three digits in order without repetition
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	int m;
+	int i;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		for (m = '0'; m <= '9'; m++)
+		{
+			for (i = '0'; i <= '9'; i++)
+			{
+				if (n != i && n < i && n != m && n < m && m != i && m < i)
+				{
+					putchar(n);
+					putchar(m);
+		        		putchar(i);
+				}
+				else
+				{
+					continue;
+				}
+				if (n < '7')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
