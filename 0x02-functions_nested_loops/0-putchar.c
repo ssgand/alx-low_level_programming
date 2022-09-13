993dd2275fc7ba0cf615afb672a9424556a99944
Steve Gandhi <sstevegandhi@gmail.com>
2022-09-13 05:07:12 -0700

    Trial

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Program to print '_putchar' on standard output
+ *
+ * Return:0
+ */
+void printing()
+{
+	printf("_putchar\n");
+}
+
+int main(void)
+{
+	printing();
+	return (0);
+}
