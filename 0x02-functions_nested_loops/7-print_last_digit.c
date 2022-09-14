ea844e89006c6c2039cc526619721414c95926ce
Steve Gandhi <sstevegandhi@gmail.com>
2022-09-14 06:49:23 -0700

    program to print every minute of a day

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - printing the last digit of a number
+ * @n: parameter
+ *
+ * Return: Sometimes 0
+ */
+int print_last_digit(int n)
+{
+	int c = n % 10;
+
+	return (c);
+}
