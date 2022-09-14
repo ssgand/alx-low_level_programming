87a041d73cca65311dca06dceb979a9662640939
Steve Gandhi <sstevegandhi@gmail.com>
2022-09-14 12:35:19 -0700

    Program to add 2 numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - program to add 2 numbers
+ *
+ * Return: Maybe 0
+ */
+int add(int a,int b)
+{
+	int t;
+
+	t = a + b;
+	return (t);
+}
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,7 +8,6 @@
  */
 int print_last_digit(int n)
 {
-	int c = n % 10;
-
-	return (c);
+	_putchar(n%10);
+	return (0);
 }
