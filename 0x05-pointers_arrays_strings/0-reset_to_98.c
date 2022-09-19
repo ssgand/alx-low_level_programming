81440e772ee36e12c2d740fdf4cb4aa036d5805b
Steve Gandhi <sstevegandhi@gmail.com>
2022-09-19 07:26:06 -0700

    program to modify a value of a variable by using its address

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *  reset_to_98 - program that resets the value of a variable
+ *   by using the value a pointer
+ *  @n: pointer that will help in reassignment
+ *
+ *  Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
