db8c07a5c5efe49c56208d85174b77aff7664bae
Steve Gandhi <sstevegandhi@gmail.com>
2022-09-20 17:48:01 -0700

    program to print the  last half of a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <string.h>
 /**
  * rev_string - program to reverse a string
  * @s: parameter
@@ -9,15 +9,16 @@
 void rev_string(char *s)
 {
 	int i, j, k = 0;
+	char c
 
 	while (s[k] != '\0')
 	{
 		k++;
 	}
 	j = k - 1;
-	for (i = j; i > 0; i--)
+	for (i = j; i >= 0; i--)
 	{
-		_putchar(s[i]);
+	
 	}
-	_putchar('\n');
+	*d = *s;
 }
