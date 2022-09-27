d5f21375673b192ffd65b96d0ab232e44810317b
Steve Gandhi <sstevegandhi@gmail.com>
2022-09-27 14:16:00 -0700

    searching for a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * strchr - program that locates a character in a string
+ * *_strchr - program that locates a character in a string
  * @s: string to be seached
  * @c: character to located
  *
@@ -10,16 +10,14 @@
 char *_strchr(char *s, char c)
 {
 	int i;
-	int *ptr;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
 		{
-			ptr = &c;
-			_putchar(ptr);
-			break;
+			return (&s[i]);
 		}
 	}
+	return (0);
 }
 
