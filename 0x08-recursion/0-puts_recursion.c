19fe3236a910c151f7c73e9419f95d766dd58f3b
Steve Gandhi <sstevegandhi@gmail.com>
2022-09-28 08:48:51 -0700

    program to add a new line to a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - program to print a string followed by a new line
+ * @s: parameter
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if(*s != '\0')
+	{
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
