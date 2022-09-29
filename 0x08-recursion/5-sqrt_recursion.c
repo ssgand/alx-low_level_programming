04840154746edd1700117e80292e5fd1f4ce8cdb
Steve Gandhi <sstevegandhi@gmail.com>
2022-09-28 17:11:38 -0700

    program to return the square root of any given number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - function to return the square root of any given number
+ * @n: parameter
+ *
+ * Return: an integer
+ */
+int _sqrt_recursion(int n)
+{
+	int x;
+	if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+	return(_sqrt(0, n));
+}
+
+/**
+ * _sqrt - function that returrn the square root of any given number
+ * @n: test number, parameter
+ * @x: squared number, parameter
+ *
+ * Return: integer
+ */
+int _sqrt(int n, int x)
+{
+	if (n > x / 2)
+		return (-1);
+	else if (n * n == x)
+		return (n);
+	return (_sqrt(n + 1, x));
+}
