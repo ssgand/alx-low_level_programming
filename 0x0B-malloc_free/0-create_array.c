5c2cb64e7499ec7ab09d444bcdb804bd682fee49
ssgand <sstevegandhi@gmail.com>
2022-10-19 23:39:02 -0700

    function to create memory space using malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * create_array - function to creat and file into an array
+ * @size: size of the array
+ * @c: character is here
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *n;
+	unsigned int i;
+
+	n = malloc((size + 1) * sizeof(char));
+
+	for(i = 0; i < size; i++)
+	{
+		if (size == 0)
+		{
+			return (NULL);
+		}
+		else
+		{
+			n[i] = c;
+		}
+	}
+	n[i] = '\0';
+	return (n);
+}		
