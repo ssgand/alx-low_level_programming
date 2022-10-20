e8bd6f3c73d486339f456f5fd2740e8fc0767868
ssgand <sstevegandhi@gmail.com>
2022-10-19 23:49:02 -0700

    Corrections

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,6 +4,8 @@
  * create_array - function to creat and file into an array
  * @size: size of the array
  * @c: character is here
+ *
+ * Return: an array
  */
 char *create_array(unsigned int size, char c)
 {
@@ -12,17 +14,18 @@ char *create_array(unsigned int size, char c)
 
 	n = malloc((size + 1) * sizeof(char));
 
-	for(i = 0; i < size; i++)
+	if (size == 0)
 	{
-		if (size == 0)
-		{
-			return (NULL);
-		}
-		else
+		return (NULL);
+	}
+
+	else
+	{
+		for (i = 0; i < size; i++)
 		{
 			n[i] = c;
 		}
 	}
 	n[i] = '\0';
 	return (n);
-}		
+}
