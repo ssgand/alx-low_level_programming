a23fa757d3528bb52fabeef5c9f46f489306f1c1
ssgand <sstevegandhi@gmail.com>
2022-10-22 08:34:00 -0700

    Corrections

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,21 +12,28 @@ char *_strdup(char *str)
 	char *ptr;
 	int n;
 	int i;
-	
-	
+
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-	else 
-		for (n = 0; str[n] != '\0'; n++);
-		
-	ptr = (char *)malloc(n + 1 * sizeof(char));
-
-	for (i = 0; str[i] != '\0'; i++)
+	else
 	{
-		ptr[i] = str[i];
+		for (n = 0; str[n] != '\0'; n++)
+			;
+		ptr = (char *)malloc(n + 1 * sizeof(char));
 	}
-	ptr[i] = '\0';	
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; str[i] != '\0'; i++)
+		{
+			ptr[i] = str[i];
+		}
+	ptr[i] = '\0';
 	return (ptr);
+	}
 }
