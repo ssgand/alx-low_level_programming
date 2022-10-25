5df6f818be0abd080ad6e196ab834c368ecde296
ssgand <sstevegandhi@gmail.com>
2022-10-25 16:29:09 -0700

    Function to return nth element

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * *get_nodeint_at_index - function to print the nth member of a linked list
+ * @head: pointer to the first member of the list
+ * @index: identification of the members of the list
+ *
+ * Return: a structure
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *t;
+	unsigned int i;
+
+	for (i = 0; i <= index; i++)
+	{
+		if (i == index)
+		{
+			t = head;
+		}
+		head = head->next;
+
+	}
+	return (t);
+}
