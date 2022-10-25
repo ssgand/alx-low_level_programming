00c8ad8ffc6e864252e59e6d00f6b8010c659234
ssgand <sstevegandhi@gmail.com>
2022-10-25 16:34:19 -0700

    Corrections

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -14,7 +14,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	for (i = 0; i <= index; i++)
 	{
-		if (i == index)
+		if (head == NULL)
+		{
+			return (NULL);
+		}
+		else if (i == index)
 		{
 			t = head;
 		}
