e155ab9e2737a587c2ccf1690abe30af18717c6b
ssgand <sstevegandhi@gmail.com>
2022-11-07 06:08:24 -0800

    Corrections 1

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,6 +13,8 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int sum = 0;
 	unsigned int i;
 
+	if (n == 0)
+		return (0);
 	va_start(p, n);
 
 	for (i = 0; i < n; i++)
---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - function to print all the parameters of a function
+ * @separator: string to separate the numbers
+ * @n: number of integer passed to the function
+ *
+ * Return: Nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
