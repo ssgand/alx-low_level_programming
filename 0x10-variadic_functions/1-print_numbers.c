021187d24e491faeab567d9dc7c5e576e141bd22
ssgand <sstevegandhi@gmail.com>
2022-11-08 01:26:49 -0800

    Function to print arguments on stdout

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -9,3 +9,25 @@
  * Return: Nothing
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list p;
+	unsigned int i;
+
+	va_start(p, n);
+	for (i = 0; i < n; i++)
+	{
+		int c = va_arg(p, int);
+
+		if (separator == NULL || i == n - 1)
+		{
+			printf("%d", c);
+		}
+		else
+		{
+			printf("%d", c);
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(p);
+}
