072fca5952dce8c293ea18e85bcdc2e55f65aac2
ssgand <sstevegandhi@gmail.com>
2022-12-08 20:20:35 +0000

    Corrections

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -11,11 +11,10 @@ size_t dlistint_len(const dlistint_t *h)
 	const dlistint_t *t = h;
 	size_t count = 0;
 
-	while (t->next != NULL)
+	while (t != NULL)
 	{
 		count++;
 		t = t->next;
 	}
-	count++;
 	return (count);
 }
