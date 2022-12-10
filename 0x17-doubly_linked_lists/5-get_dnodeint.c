c020bacb0984e8f5ad27124270416f910227848a
ssgand <sstevegandhi@gmail.com>
2022-12-10 10:42:39 +0000

    function to return a node at a specific position in a linked list

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * dlistint_t *get_dnodeint_at_index - function to return a node at a given
+ * position
+ * @head: pointer to the first node
+ * @index: position in the linked list of the node to return
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *t;
+	unsigned int count = 0;
+
+	t = head;
+	if (t == NULL)
+	{
+		return(NULL);
+	}
+	else
+	{
+		while (t != NULL)
+		{	
+			if (count == index)
+			{
+				break;
+			}
+			count++;
+			t = t->next;
+		}
+	}
+	return (t);
+}
+
