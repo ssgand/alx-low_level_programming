76a5da8d351625d5f0a0529a52bf8ccc68344bc6
ssgand <sstevegandhi@gmail.com>
2022-12-10 12:43:25 +0000

    Corrections 2

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -41,6 +41,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			count++;
 			t = t->next;
 		}
+		if (count < idx)
+		{
+			return (NULL);
+		}
 	}
 	return (pt);
 }
