cba4991a52cf2a0c8eb49715c5aed1ca5a3dd168
ssgand <sstevegandhi@gmail.com>
2022-12-14 12:25:25 +0000

    Corrections 1

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -fPIC -c *.c
-gcc -shared -o libball.so *.o
+gcc -Wall -Wextra -Werror -pedantic -c -fPIC *.c
+gcc -shared -o liball.so *.o
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
