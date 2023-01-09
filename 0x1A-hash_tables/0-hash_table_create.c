f2fd0a66524555a51f481a98a6171da4135c9d59
ssgand <sstevegandhi@gmail.com>
2023-01-09 11:50:50 +0000

    function to create a new hashtable

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - function to assign indexes to values in a hashtable
+ * @size: size of the hasttable
+ *
+ * Return: a pointer to the hashtable
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_table;
+	unsigned long int i = 0;
+
+	hash_table = malloc(sizeof(hash_table_t) * size);
+	if (hash_table == NULL)
+		return (NULL);
+	hash_table->size = size;
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (hash_table->array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		hash_table->array[i] = NULL;
+	}
+	return (hash_table);
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,1 @@
+This the readme for the hasttables assignment
